732b85f5821106fa5e56eae9044258718fbb614c
DiegoMaldonado0 <diego.maldonado5160@alumnos.udg.mx>
2024-11-11 03:53:23 -0600

    Agrega cambios en configuración y archivos necesarios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Importa los componentes desde sus respectivas carpetas
+import Navbar from './components/Navbar/Navbar';
+import HomePage from './components/HomePage/HomePage';
+import RaceDetail from './components/RaceDetail/RaceDetail';
+import DriversList from './components/DriversList/DriversList';
+import DriverDetail from './components/DriverDetail/DriverDetail';
+import DriverStandings from './components/DriverStandings/DriverStandings';
+import ConstructorStandings from './components/ConstructorStandings/ConstructorStandings';
+import RaceResults from './components/RaceResults/RaceResults';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />  {/* Aquí agregamos el componente Navbar */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/race/:id" element={<RaceDetail />} />
+          <Route path="/drivers" element={<DriversList />} />
+          <Route path="/driver/:driverId" element={<DriverDetail />} />
+          <Route path="/driver-standings" element={<DriverStandings />} />
+          <Route path="/constructor-standings" element={<ConstructorStandings />} />
+          <Route path="/raceResults/:round" element={<RaceResults />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
